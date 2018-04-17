f861010e83f6e80f0b4ec3bb6656d27891c05f8a
elton jefferson <eltonjncorreia@gmail.com>
2018-04-17 16:47:41 -0300

    rotas da api e app

--- virtus/urls.py
+++ virtus/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('virtus.core.urls')),
+    path('v1/', include('virtus.api.urls')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+
 ]
