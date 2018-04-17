7809f3438bce2245dfd01310747522ab78b127bc
elton jefferson <eltonjncorreia@gmail.com>
2018-04-17 16:46:29 -0300

    remover pedido e itens para api

--- virtus/core/models.py
+++ virtus/core/models.py
@@ -38,34 +38,3 @@ class Endereco(models.Model):
     def __str__(self):
         return self.rua
 
-
-class Pedido(models.Model):
-    numero = models.IntegerField('numero')
-    data = models.DateField('data', auto_now_add=True)
-    loja = models.CharField('loja', max_length=255)
-    cliente = models.ForeignKey('Cliente', on_delete=models.CASCADE)
-    valor_total = models.DecimalField('valor', max_digits=10, decimal_places=2,)
-
-    class Meta:
-        ordering = ['numero']
-        verbose_name = 'Pedido'
-        verbose_name_plural = 'Pedidos'
-
-    def __str__(self):
-        return self.loja
-
-
-class Item(models.Model):
-    codigo = models.IntegerField('codigo')
-    descricao = models.CharField('descricao', max_length=255)
-    valor = models.DecimalField('valor', max_digits=10, decimal_places=2,)
-    unitario = models.DecimalField('Unitário', max_digits=10, decimal_places=2)
-    quantidade = models.IntegerField('quantidade')
-
-    class Meta:
-        ordering = ['codigo']
-        verbose_name = 'Item'
-        verbose_name_plural = 'Itens'
-
-    def __str__(self):
-        return self.descricao
