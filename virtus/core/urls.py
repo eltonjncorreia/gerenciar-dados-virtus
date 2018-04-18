f7aa64f2325f07f02b6fde9c53a18359b47e644e
elton jefferson <eltonjncorreia@gmail.com>
2018-04-18 03:50:56 -0300

    implementando slug

--- virtus/core/urls.py
+++ virtus/core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import dashboard
+from .views import dashboard, edit
 
 app_name = 'core'
 urlpatterns = [
     path('', dashboard, name="dashboard"),
+    path('editar/<slug:slug>/', edit, name="edit-cliente"),
+
 ]
