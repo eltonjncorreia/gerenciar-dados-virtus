3f9d66d467c5d01c609e6c69702ed91944e3847a
elton jefferson <eltonjncorreia@gmail.com>
2018-04-18 14:13:30 -0300

    url para deletar cliente

--- virtus/core/urls.py
+++ virtus/core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import dashboard, edit
+from .views import dashboard, edit, deletar
 
 app_name = 'core'
 urlpatterns = [
     path('', dashboard, name="dashboard"),
     path('editar/<slug:slug>/', edit, name="edit-cliente"),
+    path('deletar/<slug:slug>/', deletar, name="delete"),
 
 ]
