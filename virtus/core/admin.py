0b5da4c27093726739897970946fad223c2257a1
elton jefferson <eltonjncorreia@gmail.com>
2018-04-18 12:46:22 -0300

    lisd display no admin

--- virtus/core/admin.py
+++ virtus/core/admin.py
@@ -4,6 +4,7 @@ from virtus.core.models import Cliente, Endereco
 
 class ClienteAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ['nome']}
+    list_display = ('nome', 'email', 'telefone', 'cpf')
 
 
 admin.site.register(Cliente, ClienteAdmin)
