598268de5608f6faaad7cff0ddc6ce8bfe66a41b
elton jefferson <eltonjncorreia@gmail.com>
2018-04-18 22:30:29 -0300

    form de Endereço

--- virtus/core/forms.py
+++ virtus/core/forms.py
@@ -1,9 +1,16 @@
 from django.forms import ModelForm
-from virtus.core.models import Cliente
+from virtus.core.models import Cliente, Endereco
 
 
 class ClienteModelForm(ModelForm):
 
     class Meta:
         model = Cliente
+        fields = '__all__'
+
+
+class EnderecoModelForm(ModelForm):
+
+    class Meta:
+        model = Endereco
         fields = '__all__'
